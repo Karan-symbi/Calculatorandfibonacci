115488eb50e90e1050871380210e3185ab8bb6b4
Karan-symbi <karan.chauahn.btech2023@sitpune.edu.in>
2025-02-01 22:53:30 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -29,6 +29,23 @@ class Calculator {
     double calcSqrt() {
         return Math.sqrt(input_nums[0]);
     }
+      
+    int calcFib(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num == 1) {
+            return 1;
+        }
+        int a = 0, b = 1;
+        for (int i = 2; i <= num; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+    
     
     
     
