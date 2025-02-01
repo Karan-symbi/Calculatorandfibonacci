6be02f1276ddb20b1905e67aa791fd648ca77a32
Karan-symbi <karan.chauahn.btech2023@sitpune.edu.in>
2025-02-01 22:51:31 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,13 @@
 class Calculator {
     int[] input_nums = {};
+    
     Calculator() {
         Input in = new Input();
         input_nums = in.input();
     }
-
+    
+    int calcSum() {
+        return input_nums[0] + input_nums[1];
+    }
     
 }
